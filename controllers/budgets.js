b9a257946ef842b5b34e092b0757c167b448741a
jtreeves <jackson.t.reeves@gmail.com>
2020-12-26 19:10:12 -0500

    [controllers] Create GET route for an individual budget

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -8,5 +8,20 @@ const db = require('../models')
 // Create router
 const router = express.Router()
 
+// Create GET route for budgets/test
+router.get('/test', (req, res) => {
+    res.json({msg: 'Viewing the test page for the budgets routes'})
+})
+
+// Create GET route for budgets/:id
+router.get('/:id', async (req, res) => {
+    try {
+        const currentBudget = await db.Budget.findOne({_id: req.params.id})
+        res.status(201).json({budget: currentBudget})
+    } catch(error) {
+        console.log(`BUDGET ERROR: ${error}`)
+    }
+})
+
 // Export router
 module.exports = router
