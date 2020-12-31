37554910aa33f53da5eed5c93d7daba0d2d89b96
jtreeves <jackson.t.reeves@gmail.com>
2020-12-31 14:32:49 -0500

    [tests] Create test for users/signup

--- tests/budgetTest.js
+++ tests/budgetTest.js
@@ -1,1 +1,10 @@
 // Tests for the budgets routes
+
+const assert = require('assert')
+const db = require('../models')
+
+describe('POST route for budgets/:id', () => {
+    it('should create a new budget for a user', (done) => {
+        const 
+    })
+})
--- tests/userTest.js
+++ tests/userTest.js
@@ -1,1 +1,15 @@
 // Tests for the users routes
+
+const assert = require('assert')
+const db = require('../models')
+
+describe('POST route for users/signup', () => {
+    it('should save a new user without error', (done) => {
+        const johnDoe = new db.User({
+            name: 'John Doe',
+            email: 'john@email.com',
+            password: 'john1234'
+        })
+        johnDoe.save(done)
+    })
+})
