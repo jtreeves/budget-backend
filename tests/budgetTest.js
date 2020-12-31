978db346afaf3e7edeaf1765ed452dfaea884ec8
jtreeves <jackson.t.reeves@gmail.com>
2020-12-31 14:50:49 -0500

    [tests] Add beforeEach for budgets

--- tests/budgetTest.js
+++ tests/budgetTest.js
@@ -3,6 +3,16 @@
 const assert = require('assert')
 const db = require('../models')
 
+beforeEach(async (done) => {
+    await db.User.clear()
+    const johnDoe = new db.User({
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: 'john1234'
+    })
+    johnDoe.save(done)
+})
+
 describe('POST route for budgets/:id', () => {
     it('should create a new budget for a user', (done) => {
         const 
