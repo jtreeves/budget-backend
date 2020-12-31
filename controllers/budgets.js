e63ae0d0e08a3d575cceba4483bdc071ee0697ff
jtreeves <jackson.t.reeves@gmail.com>
2020-12-31 12:30:32 -0500

    [budgets] Delete extraneous GET routes

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -14,9 +14,7 @@ const router = express.Router()
 const JWT_SECRET = process.env.JWT_SECRET
 
 // Create POST route for budgets/:id
-
 router.post('/:id', passport.authenticate('jwt', {session: false}), async (req, res) => {
-
     const { housing, utility, food, transportation, misc, income, entertainment } = req.body.categories
     try {
         const newBudget = await db.Budget.create({
@@ -68,66 +66,6 @@ router.get('/:id', passport.authenticate('jwt', {session: false}), async (req, r
     try {
         const currentBudget = await db.Budget.findOne({_id: req.params.id})
         res.status(200).json({budget: currentBudget})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/housing
-router.get('/:id/housing', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({housing: currentBudget.housing})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/utility
-router.get('/:id/utility', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({utility: currentBudget.utility})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/grocery
-router.get('/:id/grocery', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({grocery: currentBudget.grocery})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/transportation
-router.get('/:id/transportation', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({transportation: currentBudget.transportation})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/entertainment
-router.get('/:id/entertainment', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({entertainment: currentBudget.entertainment})
-    } catch(error) {
-        res.status(400).json({msg: error})
-    }
-})
-
-// Create GET route for budgets/:id/income
-router.get('/:id/income', passport.authenticate('jwt', {session: false}), async (req, res) => {
-    try {
-        const currentBudget = await db.Budget.findOne({_id: req.params.id})
-        res.status(200).json({income: currentBudget.income})
     } catch(error) {
         res.status(400).json({msg: error})
     }
