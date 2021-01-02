9099008a1404dcf64ce7300e1f4ee54e5d19132a
jtreeves <jackson.t.reeves@gmail.com>
2021-01-01 22:06:01 -0500

    [test] Add more logs for budgets/:id GET route

--- test/budgets.test.js
+++ test/budgets.test.js
@@ -78,6 +78,9 @@ describe('GET route for budgets/:id', () => {
             .get(`/budgets/${foundBudget._id}`)
             .set('Authorization', loggingUser.body.token)
         console.log(`FOUND BUDGET._ID: ${foundBudget._id}`)
+        console.log(`FOUND BUDGET._ID._BSONTYPE: ${foundBudget._id._bsontype}`)
+        console.log(`FOUND BUDGET._ID._BSONTYPE.ID: ${foundBudget._id._bsontype.id}`)
+        console.log(`FOUND BUDGET._ID._BSONTYPE KEYS: ${Object.keys(foundBudget._id._bsontype)}`)
         console.log(`GET BUDGET.BODY.BUDGET._ID: ${getBudget.body.budget._id}`)
         console.log(`GET BUDGET.BODY.BUDGET KEYS: ${Object.keys(getBudget.body.budget)}`)
         expect(getBudget).to.exist
