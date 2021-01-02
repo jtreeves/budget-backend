67bf4fecba8f608b82e1b371a9988576508d6c9b
jtreeves <jackson.t.reeves@gmail.com>
2021-01-01 22:53:17 -0500

    [test] GET route for budgets/all/:id

--- test/budgets.test.js
+++ test/budgets.test.js
@@ -86,3 +86,70 @@ describe('GET route for budgets/:id', () => {
         expect(matchBudgets).to.equal(true)
     })
 })
+
+// Test GET route for budgets/all/:id
+describe('GET route for budgets/all/:id', () => {
+    it('returns all budgets associated with a specific user', async () => {
+        
+        
+        await request(app)
+            .post('/users/signup')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({
+                name: 'David Davidson',
+                email: 'david@email.com',
+                password: 'david1234'
+            })
+        const loggingUser = await request(app)
+            .post('/users/login')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({
+                email: 'david@email.com',
+                password: 'david1234'
+            })
+        const foundUser = await db.User.findOne({
+            email: 'david@email.com'
+        })
+        const newBudget = await request(app)
+            .post(`/budgets/${foundUser._id}`)
+            .set('Authorization', loggingUser.body.token)
+            .send({
+                user: foundUser._id,
+                title: 'Test Budget',
+                colorScheme: 'Not Green',
+                categories: {
+                    housing: {inputs: {}},
+                    utility: {inputs: {}},
+                    food: {inputs: {}},
+                    transportation: {inputs: {}},
+                    entertainment: {inputs: {}},
+                    misc: {inputs: {}},
+                    income: {inputs: {}}  
+                }
+            })
+        const foundBudgets = await db.Budget.find({
+            user: foundUser._id
+        })
+        const getBudgets = await request(app)
+            .get(`/budgets/${foundUser._id}`)
+            .set('Authorization', loggingUser.body.token)
+        
+        console.log(`FOUND BUDGETS: ${foundBudgets}`)
+        console.log(`GET BUDGETS: ${getBudgets}`)
+        console.log(`FOUND BUDGETS.LENGTH: ${foundBudgets.length}`)
+        console.log(`GET BUDGETS.BODY: ${getBudgets.body}`)
+        console.log(`GET BUDGETS.BODY.BUDGET: ${getBudgets.body.budget}`)
+        console.log(`GET BUDGETS KEYS: ${Object.keys(getBudgets)}`)
+        console.log(`FOUND BUDGETS KEYS: ${Object.keys(foundBudgets)}`)
+        console.log(`GET BUDGETS KEYS: ${Object.keys(getBudgets)}`)
+        
+        
+        let matchBudgets
+        if (getBudget.body.budget._id == foundBudget._id) {
+            matchBudgets = true
+        } else {
+            matchBudgets = false
+        }
+        expect(matchBudgets).to.equal(true)
+    })
+})
