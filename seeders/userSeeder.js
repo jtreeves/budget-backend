2eadf157be8e5eb09bcacfe8d934fa2d441f7b8a
jtreeves <jackson.t.reeves@gmail.com>
2021-01-03 18:38:10 -0500

    [test] Push db objects to arrays

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -1,27 +1,27 @@
-const adam = {
+const adamUser = {
     name: 'Adam Smith',
     email: 'adam@email.com',
     password: 'adam1234'
 }
 
-const debra = {
+const debraUser = {
     name: 'Debra Halloway',
     email: 'debra@email.com',
     password: 'debra1234'
 }
 
-const john = {
+const johnUser = {
     name: 'John Doe',
     email: 'john@email.com',
     password: 'john1234'
 }
 
-const susan = {
+const susanUser = {
     name: 'Susan Green',
     email: 'susan@email.com',
     password: 'susan1234'
 }
 
-const usersList = [adam, debra, john, susan]
+const users = [adamUser, debraUser, johnUser, susanUser]
 
-module.exports = usersList
+module.exports = users
