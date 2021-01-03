33cd7914edaaaa91a6ae5a54277d046e62388057
jtreeves <jackson.t.reeves@gmail.com>
2021-01-03 01:31:40 -0500

    [seeders] Create 4 users for testing purposes

---
+++ seeders/userSeeder.js
@@ -0,0 +1,25 @@
+const adam = {
+    name: 'Adam Smith',
+    email: 'adam@email.com',
+    password: 'adam1234'
+}
+
+const debra = {
+    name: 'Debra Halloway',
+    email: 'debra@email.com',
+    password: 'debra1234'
+}
+
+const john = {
+    name: 'John Doe',
+    email: 'john@email.com',
+    password: 'john1234'
+}
+
+const susan = {
+    name: 'Susan Green',
+    email: 'susan@email.com',
+    password: 'susan1234'
+}
+
+const users = [adam, debra, john, susan]
