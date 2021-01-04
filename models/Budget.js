c912957766fed9ff91aad471ad1a78f79b0896b5
jtreeves <jackson.t.reeves@gmail.com>
2021-01-03 21:45:58 -0500

    [models] Add location field and delete income schema

--- models/Budget.js
+++ models/Budget.js
@@ -11,13 +11,14 @@ const foodSchema = require('./schemas/Food')
 const transportationSchema = require('./schemas/Transportation')
 const entertainmentSchema = require('./schemas/Entertainment')
 const miscSchema = require('./schemas/Misc')
-const incomeSchema = require('./schemas/Income')
 
 // Create Budget Schema
 const budgetSchema = new Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     title: String,
     colorScheme: String,
+    location: String,
+    income: String,
     date: {
         type: Date,
         default: Date.now()
@@ -28,8 +29,7 @@ const budgetSchema = new Schema({
         food: foodSchema,
         transportation: transportationSchema,
         entertainment: entertainmentSchema,
-        misc: miscSchema,
-        income: incomeSchema
+        misc: miscSchema
     },
 })
 
